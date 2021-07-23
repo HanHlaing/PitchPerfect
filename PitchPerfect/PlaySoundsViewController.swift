56640d477fae6f8dcdf1ad7b5267f7342545c19d
Han Hlaing Moe <hanhlaingmoe.hhm27@gmail.com>
2021-07-23 15:36:38 +0630

    Add the audio extension and play sounds

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -10,6 +10,7 @@ import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
 
+    // MARK: Outlets
     
     @IBOutlet weak var btnSnail: UIButton!
     @IBOutlet weak var btnRabbit: UIButton!
@@ -25,11 +26,42 @@ class PlaySoundsViewController: UIViewController {
     var audioPlayerNode: AVAudioPlayerNode!
     var stopTimer: Timer!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    enum ButtonType: Int { case slow = 0, fast, chipmunk, vader, echo, reverb}
+    
+    // MARK: Actions
 
-        // Do any additional setup after loading the view.
+    @IBAction func playSoundForButton(_ sender: UIButton) {
+        switch(ButtonType(rawValue: sender.tag)!) {
+           case .slow:
+               playSound(rate: 0.5)
+           case .fast:
+               playSound(rate: 1.5)
+           case .chipmunk:
+               playSound(pitch: 1000)
+           case .vader:
+               playSound(pitch: -1000)
+           case .echo:
+               playSound(echo: true)
+           case .reverb:
+               playSound(reverb: true)
+           }
+
+           configureUI(.playing)
     }
     
+    
+    @IBAction func stopButtonPressed(_ sender: Any) {
+        stopAudio()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupAudio()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        configureUI(.notPlaying)
+    }
 
 }
